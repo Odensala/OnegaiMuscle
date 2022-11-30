fda8a5a55aeae43d0f84922396724c357066d455
Odensala <paulpark.dev@gmail.com>
2022-11-30 13:13:54 +0100

    test: modified database unit tests to use Dagger Hilt

--- app/src/androidTest/java/com/odensala/onegaimuscle/data/ExerciseDaoTest.kt
+++ app/src/androidTest/java/com/odensala/onegaimuscle/data/ExerciseDaoTest.kt
@@ -1,33 +1,35 @@
 package com.odensala.onegaimuscle.data
 
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 @OptIn(ExperimentalCoroutinesApi::class)
-@RunWith(AndroidJUnit4::class)
 @SmallTest
+@HiltAndroidTest
 class ExerciseDaoTest {
 
-    private lateinit var database: ExerciseDatabase
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    @Named("test_database")
+    lateinit var database: ExerciseDatabase
     private lateinit var dao: ExerciseDao
 
     @Before
     fun setup() {
-        database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            ExerciseDatabase::class.java
-        ).allowMainThreadQueries().build()
-
+        hiltRule.inject()
         dao = database.exerciseDao()
     }
 
